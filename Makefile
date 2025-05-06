5b1e312aab4da9e2e5dd66d2e5f4b0b99d1edbe1
Maksym Oliinyk <maoliiny@c2r2p13.hive.fi>
2025-05-06 12:09:11 +0300

    parsing and validation is complete

---
+++ Makefile
@@ -0,0 +1,37 @@
+NAME = push_swap
+
+SRCS = push_swap.c
+#BONUS_C = ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c ft_lstlast_bonus.c ft_lstadd_back_bonus.c ft_lstdelone_bonus.c ft_lstclear_bonus.c ft_lstiter_bonus.c ft_lstmap_bonus.c 
+
+#BONUS = ${BONUS_C:.c=.o}
+#.SECONDARY: ${BONUS}
+
+OBJS = ${SRCS:.c=.o}
+.SECONDARY: ${OBJS}
+
+CC = cc
+
+RM = rm -f
+
+CFLAGS = -Wall -Wextra -Werror
+
+all:	${NAME}
+
+.c.o:
+	${CC} ${CFLAGS} -I. -c $< -o ${<:.c=.o}
+
+$(NAME):
+	${CC} ${CFLAGS} ${SRCS} libft.a -I. -o ${NAME}
+
+run: ${NAME}
+	./${NAME} 2 1 3 2345235 7 9 1 23 123 618765871
+
+clean:
+	${RM} ${NAME}
+
+fclean:	clean
+	${RM} ${NAME}
+	
+re:		fclean all
+
+.PHONY: all clean fclean re
--- push_swap.c
+++ push_swap.c
@@ -6,32 +6,58 @@
 /*   By: maoliiny <maoliiny@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 15:15:41 by maoliiny          #+#    #+#             */
-/*   Updated: 2025/05/05 18:55:59 by maoliiny         ###   LAUSANNE.ch       */
+/*   Updated: 2025/05/06 12:08:18 by maoliiny         ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_max/libft.h"
 
+static int	is_duplicate(const long *a, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (a[i] == a[j])
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
 int	str_isdigit(const char *s)
 {
 	int	len;
+	int	i;
 
+	i = 0;
 	len = 0;
-	if (*s == '-' || *s == '+')
+	if ((s[i] == '-' || s[i] == '+') && ft_strlen(s) > 1)
 	{
 		len++;
-		s++;
+		i++;
 	}
-	while (*s)
-		if (ft_isdigit(*s++) == 0)
+	while (s[i])
+		if (ft_isdigit(s[i++]) == 0)
 			return (0);
-	if (ft_strlen(s) - len < 11)
+	if (ft_strlen(s) - len < 13)
 		return (1);
 	return (0);
 }
 
-void	print_error_and_exit(void)
+void	print_error_and_exit(long *a, long *b)
 {
+	if (a)
+		free(a);
+	if (b)
+		free(b);
 	write(2, "Error\n", 6);
 	exit(EXIT_FAILURE);
 }
@@ -41,31 +67,42 @@ long	*fill_a(int ac, char **av)
 	long	*a;
 	int		i;
 
-	a = malloc(sizeof(int) * ac);
+	a = malloc(sizeof(long) * ac);
 	i = 0;
 	if (!a)
-		return (NULL);
+		print_error_and_exit(NULL, NULL);
 	while (i < ac)
 	{
 		if (str_isdigit(av[i]))
 		{
 			a[i] = ft_atoi(av[i]);
 			if (a[i] > 2147483647 || a[i] < -2147483648)
-				print_error_and_exit();
+			{
+				print_error_and_exit(a, NULL);
+			}
 		}
+		else
+			print_error_and_exit(a, NULL);
 		i++;
 	}
+	if (is_duplicate(a, ac))
+		print_error_and_exit(a, NULL);
 	return (a);
 }
 
 int	main(int ac, char **argv)
 {
 	long	*a;
+	long	*b;
+	int		i;
 
+	i = 0;
 	if (ac < 2)
 		return (0);
 	a = fill_a(ac - 1, &argv[1]);
-	for (int i = 0; i < ac - 1; i++)
-		ft_printf("num: %i\n", a[i]);
+	b = malloc(sizeof(a) * (ac - 1));
+	while (i < ac - 1)
+		ft_printf("num: %i\n", a[i++]);
 	free(a);
+	free(b);
 }
